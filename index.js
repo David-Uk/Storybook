6e41181dcfac1f79ecb1f5c78bf028c66b498c28
CHidave291 <dukelere@gmail.com>
2019-12-28 02:41:04 +0100

    Setup oauth

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const auth = require('./routes/auth');
+const passport = require('passport');
+require('./config/passport')(passport);
 
 const app = express();
+
+mongoose.connect('mongodb+srv://scottadkins:TyRQvprolKjZyRVQ@cluster0-smlfc.mongodb.net/test?retryWrites=true&w=majority', { useUnifiedTopology: true })
+	.then(() => {
+		console.log('MongoDB connected!');
+	});
+
+app.use('/auth', auth);
 
 app.get('/', (req, res) => {
 	res.send('Storybook');
