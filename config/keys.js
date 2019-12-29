345d5fce4a8ef2198fc8e1c35e02fb6682bcad9d
CHidave291 <dukelere@gmail.com>
2019-12-29 02:17:10 +0100

    Start UI

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-	mongoURI: 'mongodb+srv://scottadkins:TyRQvprolKjZyRVQ@cluster0-smlfc.mongodb.net/test?retryWrites=true&w=majority',
-	googleClientID: '1008947621223-7gf33jbsrvkftaj2i0coikqcatotm2tj.apps.googleusercontent.com',
-	googleClientSecret: 'hmLIk9GhUQxNaDOGefOVN3Bj'
+  mongoURI:'mongodb+srv://scottadkins:TyRQvprolKjZyRVQ@cluster0-smlfc.mongodb.net/test?retryWrites=true&w=majority',
+  googleClientID:'1008947621223-7gf33jbsrvkftaj2i0coikqcatotm2tj.apps.googleusercontent.com',
+  googleClientSecret:'hmLIk9GhUQxNaDOGefOVN3Bj'
 }
