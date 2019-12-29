75eb0392e66bc69539c9721a9c8602ddeeebd81a
CHidave291 <dukelere@gmail.com>
2019-12-29 16:59:11 +0100

    Completed UI

---
+++ routes/stories.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const { ensureAuthenticated, ensureGuest } = require('../helpers/auth');
+
+router.get('/', (req, res) => {
+	res.render('stories/index');
+})
+
+router.get('/add', ensureAuthenticated, (req, res) => {
+	res.render('stories/add');
+})
+
+
+
+module.exports = router;
